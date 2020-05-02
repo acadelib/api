5ef06417b5aee2661327bf2638117d08f48da72a
Samuel Maurice <samuel@acadelib.com>
2020-05-02 17:56:57 +0200

    Ajout de l'authentification

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('login', 'Auth\LoginController@login')->name('login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
