0c093e16a202b33c43bf9f5e90f2f280f4bc11d0
Samuel Maurice <samuel@acadelib.com>
2022-05-10 23:00:05 +0200

    refactor: Utilisation des migrations anonymes

--- database/migrations/2020_05_06_192917_create_school_years_table.php
+++ database/migrations/2020_05_06_192917_create_school_years_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSchoolYearsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -32,4 +32,4 @@ class CreateSchoolYearsTable extends Migration
     {
         Schema::dropIfExists('school_years');
     }
-}
+};
